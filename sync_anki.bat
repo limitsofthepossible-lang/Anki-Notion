e49382335ddfb3a7c24b7096c1c87d22dfcf05da
limitsofthepossible-lang <limitsofthepossible@gmail.com>
2026-09-04 15:24:14 +1200

    Update sync_anki.bat

--- sync_anki.bat
+++ sync_anki.bat
@@ -1,6 +1,6 @@
 @echo off
 set GITHUB_REPO=YOUR-GITHUB-USERNAME/anzca-anki-dashboard
 set GITHUB_TOKEN=PASTE-YOUR-TOKEN-HERE
-set ANKI_ROOT_DECKS=My ANZCA primary;Pharmacology
+set ANKI_ROOT_DECKS=ANZCA primary;Pharmacology
 python sync_anki.py
 pause
