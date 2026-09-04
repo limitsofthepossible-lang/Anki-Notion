3b2942f4db67cb7c9b89e9c70ec5681a3eadcc2d
limitsofthepossible-lang <limitsofthepossible@gmail.com>
2026-09-04 15:32:42 +1200

    Add files via upload

--- sync_anki.bat
+++ sync_anki.bat
@@ -1,6 +1,6 @@
 @echo off
 set GITHUB_REPO=YOUR-GITHUB-USERNAME/anzca-anki-dashboard
 set GITHUB_TOKEN=PASTE-YOUR-TOKEN-HERE
-set ANKI_ROOT_DECKS=ANZCA primary;Pharmacology
+set ANKI_ROOT_DECKS=ANZCA Primary;Pharmacology
 python sync_anki.py
 pause
